3e58b49fef6f890c619ce613dd540765c1258897
ami3 <riverknuuttila2@outlook.com>
2025-11-15 01:12:12 -0500

    Update main.go
    added functionality to move a player character left and right.

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ var ( //declvare variable for images, name *ebiten.Image.
 	//charecter2 * ebiten.Image
 	screenWidth = 750
 	screenHeight = 750
-	player1InitW = (screenWidth / 2) + 50
-	player1InitH = (screenHeight / 2) + 50
-	//player2InitW = (screenWidth / 2) - 50
-	//player2InitH = (screenHeight / 2) - 50
+	player1InitX = float64((screenWidth / 2) + 50)
+	player1InitY = float64((screenHeight / 2) + 50)
+	//player2InitX = (screenWidth / 2) - 50
+	//player2InitY = (screenHeight / 2) - 50
 )
 
 func init() { //initialize images to variables here.
@@ -26,7 +26,7 @@ func init() { //initialize images to variables here.
 		log.Fatal(err)
 	}
 
-	//player1, _, err = ebitenutil.NewImageFromFile("") //will not run if empty
+	player1, _, err = ebitenutil.NewImageFromFile("assets/images/Sprite-0001.png") //will not run if empty
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,11 +35,28 @@ func init() { //initialize images to variables here.
 type Game struct{}
 
 func (g *Game) Update() error {
+	
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) == true {
+		player1InitX = player1InitX + 2
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) == true {
+		player1InitX = player1InitX - 2
+	}
+
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (g *Game) Draw(screen *ebiten.Image) {  //called every frame, game tick rate
 	screen.DrawImage(background, nil)
+
+	x := player1InitX
+	y := player1InitY
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(x,y)
+
+	screen.DrawImage(player1, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -49,7 +66,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(screenHeight, screenWidth)
 	ebiten.SetWindowTitle("Render an image")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	
+	if err := ebiten.RunGame(&Game{}); err != nil { 
 		log.Fatal(err)
 	}
+	
 }
