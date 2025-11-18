b218c3526d2eecc7beda8ecb2dcfffaf8e9f78d6
annie val <riverknuuttila2@outlook.com>
2025-11-18 13:21:38 -0500

    moved functions into superate functions.go file

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"math/rand"
 	"log"
 	"fmt"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-
-
-func randInt(min, max int) int {
-	return rand.Intn(max-min+1) + min
-}
 
 var ( //declvare variable for images, name *ebiten.Image.
 	background *ebiten.Image
@@ -45,82 +39,6 @@ type axeZombie struct{
 	x, y 		float64
 	speed		float64
 	}
-
-
-
-func spawnZombies() {
-  count := randInt(3, 6)
-
-  for i := 0; i < count; i++ {
-    z := axeZombie{
-    x:     randFloat(0, float64(screenWidth + 100)),
-    y:     randFloat(0, float64(screenHeight + 100)),
-    hp:    randInt(3, 10),
-    level: randInt(1, 3),
-    speed: randFloat(0.3, 1.0),
-    }
-    
-		zombies = append(zombies, z)
-  }
-}
-
-func enemyMovement(targetX, targetY, enemyX, enemyY, speed float64) (float64, float64) {
-	if enemyX < (targetX - 80){ //enemie movement
-		enemyX += speed
-	}
-	if enemyX > (targetX + 80){
-		enemyX -= speed
-	}
-	if enemyY < (targetY - 80){
-		enemyY += speed
-	}
-	if enemyY > (targetY + 80){
-		enemyY -= speed
-	}
-
-	return enemyX, enemyY
-}
-
-func loadAxeZombieSprites() {
-  axeZombieSprites = make([]*ebiten.Image, 8)
-
-  for i := 1; i <= 8; i++ {
-    filename := fmt.Sprintf("assets/sprites/enemies/axeZombie/axeZombieSprite%02d.png", i)
-
-    img, _, err := ebitenutil.NewImageFromFile(filename)
-    if err != nil {
-    log.Fatal(err)
-    }
-    axeZombieSprites[i-1] = img
-	}
-}
-
-func abs(f float64) float64 {
-  if f < 0 {
-    return -f
-  }
-  return f
-}
-
-func randFloat(min, max float64) float64 {
-  return min + rand.Float64()*(max-min)
-}
-
-
-func isBlocked(px, py float64, dx, dy float64, blockRange float64, zombies []axeZombie) bool {
-  for _, z := range zombies {
-  	// Project the check range in the direction the player wants to move
-    checkX := px + dx*blockRange
-    checkY := py + dy*blockRange
-
-    // If an enemy is near that projected point → blocked
-    if abs(z.x-checkX) < 50 && abs(z.y-checkY) < 50 {
-      return true
-    }
-  }
- 
-	return false
-}
 
 func init() { //initialize images to variables here.
 	var err error
